cfb7635867058867e89269fd734d1c07bb9dad25
kinjal <kinjaldobariya394@gmail.com>
2022-07-20 15:00:53 -0400

    update reg page

--- src/temp/RegPage.java
+++ src/temp/RegPage.java
@@ -5,6 +5,8 @@ public class RegPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Reg page");
+		System.out.println("add selenium webdriver");
+
 
 	}
 
